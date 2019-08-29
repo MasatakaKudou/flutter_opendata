42bd2f75541a8a3b037cd42a8cd67b1db8f0d8d0
MasatakaKudou <b1017027@fun.ac.jp>
2019-08-30 00:28:14 +0900

    画面に表示できた

--- lib/config.dart
+++
@@ -1,5 +0,0 @@
-// API_KEY for Open Data Challenge for Public Transportation in Tokyo
-const String apikey_opendata = '<your api key>';
-
-// contact email
-const String contact_email = 'address@domain';
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:location/location.dart';
-import 'package:latlong/latlong.dart';
-
-import 'config.dart';
-import 'odpt.dart';
+import 'dart:async' show Future;
+import 'package:csv/csv.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 const String app_title ='近くの駅';
 const String term_of_use =
-    '本アプリケーション等が利用する公共交通データは、'
-    '東京公共交通オープンデータチャレンジにおいて提供されるものです。'
+    '本アプリケーション等が利用するデータは、'
+    '函館市の公式サイトによって、提供されるものです。'
     '公共交通事業者により提供されたデータを元にしていますが、'
     '必ずしも正確・完全なものとは限りません。本アプリケーションの表示内容について、'
-    '公共交通事業者への直接の問合せは行わないでください。'
-    '本アプリケーションに関するお問い合わせは、以下のメールアドレスにお願いします。'
-    '\n\n$contact_email';
+    '本アプリケーションに関するお問い合わせは、以下のメールアドレスにお願いします。 \n'
+    'nisino7se@gmail.com';
 
 
 void main() => runApp(MyApp());
@@ -44,77 +40,28 @@ class OpenDataState extends State<OpenData> {
 
   GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  List<String> _currentListView;
+  List<List<dynamic>> data = [];
 
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
+  loadCSV() async {
+    final myData = await rootBundle.loadString("assets/SalesJan2009.csv");
+    List<List<dynamic>> csvTable = CsvToListConverter().convert(myData);
 
-  initPlatformState() async {
-    // Setup location　// 解説1
-    final Location location = Location();
-
-    await location.changeSettings(
-        accuracy: LocationAccuracy.HIGH, interval: 5000);
-    try {
-      bool _serviceStatus = await location.serviceEnabled();
-      print("Service status: $_serviceStatus");
-      if (!_serviceStatus) {
-        bool serviceStatusResult = await location.requestService();
-        print("Service status activated after request: $serviceStatusResult");
-        if (serviceStatusResult) {
-          initPlatformState();
-        }
-        return;
-      }
-      bool permission = await location.requestPermission();
-      print("Permission: $permission");
-      if (!permission) return;
-    } on PlatformException catch (e) {
-      print(e);
-      return;
-    }
-
-    await for(final LocationData currentLocation
-    in location.onLocationChanged()) {  // 解説2
-      print('Handling location stream at:${DateTime.now()}');
-      // Setup String List for ListView
-      List<String> listString = [];
-      final Odpt odpt = Odpt();
-      List<OdptStation> listStation =
-      await odpt.placesStation(currentLocation); // 解説3
-      if (listStation == null) {
-        print('There is no or an error response from DDPT-API.');
-        listString.add('オープンデータAPIにアクセスできないか、エラーが返却されました');
-      } else if (listStation.isEmpty) {
-        print('There is no station around here.');
-        listString.add('近くに駅はみつかりませんでした');
-      } else {
-        final Distance distance = Distance();
-        for(OdptStation element in listStation) {
-          listString.add(
-              '${element.dcTitle}: '
-                  '${distance(LatLng(currentLocation.latitude,
-                  currentLocation.longitude),
-                  LatLng(element.geoLat, element.geoLong)).toString()}m'
-          );
-        }
-      }
-      setState(() {  // 解説4
-        _currentListView = listString;
-      });
-    }
+    data = csvTable;
   }
 
   @override
-  @override
-  Widget build(BuildContext context) => DefaultTabController(  // 解説5
+  Widget build(BuildContext context) => DefaultTabController(
     length: 2,
     initialIndex: 0,
     child: Scaffold(
       key: _scaffoldKey,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          onPressed: () async {
+            await loadCSV();
+            print(data);
+          }),
       appBar: AppBar(
           title: Text(widget.title),
           bottom: TabBar(
@@ -127,14 +74,27 @@ class OpenDataState extends State<OpenData> {
 
       body: TabBarView(
         children: <Widget>[
-          _currentListView == null
-              ? CircularProgressIndicator()
-              : ListView.builder(   // 解説6
-              itemCount: _currentListView.length,
-              itemBuilder: (context, int index) => Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text(_currentListView[index]),
-              )
+          Table(
+            columnWidths: {
+              0: FixedColumnWidth(100.0),
+              1: FixedColumnWidth(200.0),
+            },
+            border: TableBorder.all(width: 1.0),
+            children: data.map((item) {
+              return TableRow(
+                  children: item.map((row) {
+                    return Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          row.toString(),
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                    );
+                  }).toList()
+              );
+            }).toList(),
           ),
           Padding(
             padding: EdgeInsets.all(16.0),
